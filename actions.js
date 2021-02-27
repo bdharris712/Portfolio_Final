a10d25f07b131da999782ee825739fa8472ad400
B3gg4r <bdharris712@gmail.com>
2021-02-27 13:31:46 -0700

    don't know what i changed
    Having some major issues with not being able to publish this repo

--- actions.js
+++ actions.js
@@ -1,14 +1,5 @@
-$(".experiment").click(function(e) {
-    e.preventDefault();
-    for (var i = 0; i < 3; i++ ) {
-        $(".experiment")
-            .animate( { backgroundColor: "#f00" }, 2000 )
-            .animate( { backgroundColor: "transparent" }, 2000 );
-    }
-});
-
-$(document).ready(function(){
-    $(".pulse-button").click(function(){
-      $(".resume").slideToggle();
-    });
-  });
+// $(document).ready(function(){
+//     $(".pulse-button").click(function(){
+//       $(".resume").slideToggle();
+//     });
+//   });
